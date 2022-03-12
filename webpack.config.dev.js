5c200a00225ae92f2f852e4822439ecccd5b96bb
codigoenlaweb <codeenlaweb@gmail.com>
2022-03-12 18:49:07 -0400

    webpack server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -58,4 +58,17 @@ module.exports = {
       filename: 'assets/[name].[contenthash].css'
     }),
   ],
+  devServer: {
+    static: 
+    {
+      directory: path.join(__dirname, "dist"),
+      watch: true,
+    },
+    watchFiles: path.join(__dirname, "./**"), //observa los cambios en todos nuestros archivos y actualiza el navegador
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
+    open: true, //Hace que se abra en el navegador
+    
+  },
 }
