be4006752246d6de9b5f18f14a6a28ede5ee79d0
codigoenlaweb <codeenlaweb@gmail.com>
2022-03-26 15:10:23 -0400

    i'm

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,7 @@ const About = () => {
         <h2 class="text-xl md:text-2xl text-cyan-600 mb-4 font-bold font-mono animate-pulse">A world of magic behind a button</h2>
         <img class="contrast-100 md:w-1/3 max-w-xs rounded-lg" src="${olmos}" alt="profile">
 
-        <h2 id='msmMachineWriteComplete' class="mt-4 md:mt-6 text-pink-600 text-lg md:text-xl font-mono transition duration-300 ease-linear">¡Hi!, i'm Jesús Olmos<p id="machine-write"></p></h2>
+        <h2 id='msmMachineWriteComplete' class="mt-4 md:mt-6 text-pink-600 text-lg md:text-xl font-mono transition duration-300 ease-linear">¡Hi!, I'm Jesús Olmos<p id="machine-write"></p></h2>
 
         <h3 class="md:mt-2 text-gray-600">
           I am a full stack developer focused on the back-end with experience in web development.
