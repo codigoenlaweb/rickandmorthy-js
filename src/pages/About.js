eec4bff76e98e572675f38bb6542a590e77fdd7d
codigoenlaweb <codeenlaweb@gmail.com>
2022-03-25 23:28:59 -0400

    delete >

--- src/pages/About.js
+++ src/pages/About.js
@@ -37,7 +37,7 @@ const About = () => {
     <footer class=" bg-neutral-800 py-2 px-4 sm:px-8 sm:py-4">
       <h4 class="mb-2 md:mb-4 text-lg font-bold text-white text-center">Jesus Olmos | Codigoenlaweb</h4>
       <ul class="w-full flex flex-col items-center sm:flex-row flex-wrap sm:justify-around">
-        <li class="text-gray-300 pr-1" >+58 412-0529358></li>
+        <li class="text-gray-300 pr-1">+58 412-0529358</li>
         <li class="text-gray-300 px-1">olmosjesus@gmail.com</li>
         <li class="px-1"><a class="text-gray-300 hover:text-white transition duration-150 ease-linear" href="https://github.com/codigoenlaweb" target="_blank">Github</a></li>
         <li class="pl-1"><a class="text-gray-300 hover:text-white transition duration-150 ease-linear" href="http://www.linkedin.com/in/jesus-armando-olmos-olmos-607748228" target="_blank">Linkedin</a></li>
